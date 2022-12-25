1796db661010859ad6ac1f614fb5fab0ceeba841
oridr <admin@codeocean.com>
2022-12-25 13:20:53 +0000

    temp

---
+++ data/cats-v1/data/dataset_json.js
@@ -0,0 +1,19 @@
+{
+    "bucket": {
+        "origin": "aws",
+        "bucket": "ori-ext-dset-test-bucket",
+        "prefix": ""
+    },
+    "aws": {
+        "region": "us-east-1",
+        "accessKeyID": "YOUR ACCESS KEY",
+        "accessKeySecret": "YOUR SECRET KEY"
+    },
+    "name" : "dataset from api",
+    "description" : "a dataset created from external api",
+    "tags": ["my tag", "another tag"],
+    "mount": "dataset",
+    "external": true,
+    "indexExternal": true
+}
+
